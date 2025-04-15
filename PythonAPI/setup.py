5d3be64e786f51846744262ea8fb81fa3984acbf
Joseph Lu <josephlu414@gmail.com>
2025-04-15 17:11:32 -0400

    Adjusted to work for Windows

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,5 +1,6 @@
 from setuptools import setup, Extension
 import numpy as np
+import sys
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
@@ -9,7 +10,7 @@ ext_modules = [
         'pycocotools._mask',
         sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
         include_dirs = [np.get_include(), '../common'],
-        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
+        extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'] if not sys.platform == 'win32' else [],
     )
 ]
 
