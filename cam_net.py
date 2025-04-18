dc1bee99dd17cd68b3266e0ec50fb2f7bb1cf797
Jerry Guo <160366609+guojerry3@users.noreply.github.com>
2025-04-17 22:41:56 -0400

    use GoogLeNet

--- cam_net.py
+++ cam_net.py
@@ -1,7 +1,8 @@
 import numpy as np
 import torch.nn as nn
 import torch
-
+import torchvision.models as models
+from torchvision.models.googlenet import GoogLeNet_Weights
 
 class AlexNet_GAP(nn.Module):
     def __init__(self, num_classes):
@@ -32,7 +33,7 @@ class AlexNet_GAP(nn.Module):
         )
 
         self.gap = nn.AdaptiveAvgPool2d((1, 1))
-        self.classifier = nn.Linear(256, num_classes)
+        self.classifier = nn.Linear(1024, num_classes)
 
     def forward(self, x):
         x = self.features(x)
@@ -40,3 +41,42 @@ class AlexNet_GAP(nn.Module):
         x = self.gap(x)
         x = torch.flatten(x, 1)
         return self.classifier(x)
+
+
+
+class GoogLeNet_GAP(nn.Module):
+    def __init__(self, num_classes=2):
+        super(GoogLeNet_GAP, self).__init__()
+
+        # Load pretrained GoogLeNet
+        original_model = models.googlenet(weights=GoogLeNet_Weights.IMAGENET1K_V1)
+
+        # Truncate after inception4e
+        self.features = nn.Sequential(
+            original_model.conv1,
+            original_model.maxpool1,
+            original_model.conv2,
+            original_model.conv3,
+            original_model.maxpool2,
+            original_model.inception3a,
+            original_model.inception3b,
+            original_model.maxpool3,
+            original_model.inception4a,
+            original_model.inception4b,
+            original_model.inception4c,
+            original_model.inception4d,
+            original_model.inception4e
+        )
+        # Additions from the paper
+        self.conv = nn.Conv2d(832, 1024, kernel_size=3, stride=1, padding=1)
+        self.gap = nn.AdaptiveAvgPool2d((1, 1))
+        self.classifier = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.conv(x)
+        self.feature_maps = x
+        x = self.gap(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
